4442203e382a199c6931b29afdeb2538c1e07da5
stevglobal <stephencharlesjr36@gmail.com>
2025-08-15 10:14:20 +0400

    my first commit

---
+++ my_list.py
@@ -0,0 +1,25 @@
+my_list = []
+#append numbers to my_list
+my_list.append(10,)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+#insert the value 15 at the second position in  the list.
+my_list.insert(1,15)
+
+#extend my_list with numbers
+my_list.extend([50,60,70])
+
+#remove the last element from my_list
+my_list.pop()
+
+#sort my_list in ascending order
+my_list.sort()
+
+#find and print the index of value 3o
+my_list.index(30)
+
+#display results
+print(my_list)
+print(my_list.index(30))
